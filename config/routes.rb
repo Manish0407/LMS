2d80bcf58de992721cbee2ff3a351d75a1c790b4
Manish0407 <manishp@shriffle.com>
2022-08-17 20:20:09 +0530

    this is day5

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,20 @@
 Rails.application.routes.draw do
+  get 'static_pages/landing_page'
+  get 'static_pages/dashboard'
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-   root "home#index"
-
+   root 'static_pages#landing_page'
+   # resources :users
   # get "/articles", to: "articles#index"
   get 'home', to: 'home#index'
   get 'student', to: 'student#student'
   get 'books', to: 'books#books'
+  get 'user/:id', to: 'user#show'
   get 'sign_in', to: 'user#signin'
   get 'sign_up', to: 'user#signup'
   get 'contact', to: 'home#contact'
   get 'about', to: 'home#about'
+  #delete 'users/sign_out', to: 'user#signin'
 end
--- db/migrate/20220810080720_create_users.rb
+++ db/migrate/20220810080720_create_users.rb
@@ -1,6 +1,9 @@
 class CreateUsers < ActiveRecord::Migration[7.0]
   def change
-    create_table :users do |t|
+    create_table :user do |t|
+      t.string :f_name
+      t.string :l_name
+      t.string :user_type
       t.string :username
       t.string :password
       t.timestamps
